0c847b1bb418ee6643485badc99494156f849458
ruoruochen <2679330388@qq.com>
2021-01-07 16:43:42 +0800

    实现表单的输入框布局、数据绑定及验证、以及重置功能

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
-
+import './assets/fonts/iconfont.css'
 Vue.config.productionTip = false
 
 new Vue({
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-
+import { Button, Form, FormItem, Input } from 'element-ui'
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)

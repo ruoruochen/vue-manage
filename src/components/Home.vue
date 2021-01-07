1426a153b4aeb138b044ff3ade94daaeb04b0673
ruoruochen <2679330388@qq.com>
2021-01-07 22:34:36 +0800

    完成主页布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home_container">
+    <!-- 头部区域 -->
+    <el-header class="header">
+      Header
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主题区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 主题内容 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -19,5 +30,20 @@ export default {
 };
 </script>
 
-<style>
+<style lang="less" scoped>
+.home_container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #363d40;
+}
+
+.el-aside {
+  background-color: #313743;
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 Vue.prototype.$message = Message

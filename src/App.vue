be1157bb2870cba61600cccf15e8a414717a88c9
ruoruochen <2679330388@qq.com>
2021-01-07 21:14:23 +0800

    解决冲突

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,14 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app"
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import './assets/fonts/iconfont.css'
+import axios from "axios"
+Vue.prototype.$http = axios
+// 配置请求根路径
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
 
 Vue.config.productionTip = false
 

efacbd92e3fded5ee891646f35a25885165c08a8
ruoruochen <2679330388@qq.com>
2021-01-07 21:07:18 +0800

    Revert "实现表单的输入框布局、数据绑定及验证、以及重置功能"
    This reverts commit 0c847b1bb418ee6643485badc99494156f849458.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,75 +1,16 @@
 <template>
   <div class="login_container">
     <div class="login_box">
-      <!-- 头像区 -->
       <div class="login_head">
         <img src="../assets/images/logo.png" alt />
       </div>
-      <!-- 登录表单区 -->
-      <el-form
-        label-width="0"
-        ref="loginFormRef"
-        class="login_form"
-        :rules="loginRules"
-        :model="loginForm"
-      >
-        <!-- 用户名 -->
-        <el-form-item prop="username">
-          <el-input prefix-icon="iconfont icon-yonghuming
-" v-model="loginForm.username"></el-input>
-        </el-form-item>
-        <!-- 密码 -->
-        <el-form-item prop="password">
-          <el-input prefix-icon="iconfont icon-mima" v-model="loginForm.password" type="password"></el-input>
-        </el-form-item>
-        <!-- 按钮区 -->
-        <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
-          <el-button type="info" @click="reset">重置</el-button>
-        </el-form-item>
-      </el-form>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Login",
-  data() {
-    return {
-      loginForm: {
-        username: "",
-        password: ""
-      },
-      loginRules: {
-        // 验证用户名是否合法
-        username: [
-          {
-            required: true,
-            message: "请输入登录名称",
-            trigger: "blur"
-          },
-          {
-            min: 6,
-            max: 16,
-            message: "用户名长度需为6~16位之间",
-            trigger: "blur"
-          }
-        ],
-        // 验证密码是否合法
-        password: [
-          { required: true, message: "请输入密码", trigger: "blur" },
-          { min: 6, max: 16, message: "密码需要在6~16位之间", trigger: "blur" }
-        ]
-      }
-    };
-  },
-  methods: {
-    reset() {
-      // console.log(this);
-      this.$refs.loginFormRef.resetFields();
-    }
-  }
+  name: "Login"
 };
 </script>
 
@@ -110,17 +51,5 @@ export default {
       background-color: #eee;
     }
   }
-}
-.login_form {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  padding: 0 20px;
-  box-sizing: border-box;
-}
-
-.btns {
-  display: flex;
-  justify-content: flex-end;
 }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,4 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input } from 'element-ui'
+import { Button } from 'element-ui'
+
 Vue.use(Button)
-Vue.use(Form)
-Vue.use(FormItem)
-Vue.use(Input)
